d4aa50628e2a20fe81e7a8a4745bce289f42e99b
Luc Joosten <lhajoost@avans.nl>
2020-04-30 20:17:46 +0200

    starting up basic structure of front-end app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="navbar">
+			<Navbar />
+		</div>
+
+		<div class="sidebar">
+			<Sidebar />
+		</div>
+
+		<div class="view">
+			<router-view />
+		</div>
+
+		<div class="footer">
+			<Footer />
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import Navbar from "./components/basic/Navbar";
+	import Sidebar from "./components/basic/Sidebar";
+	import Footer from "./components/basic/Footer";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: {
+			Navbar,
+			Sidebar,
+			Footer
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	/* #app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	} */
 </style>
