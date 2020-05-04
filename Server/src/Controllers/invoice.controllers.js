289a9ae254fc4238bd8484d1a1ea31ea128221ee
Luc Joosten <lhajoost@avans.nl>
2020-05-04 22:46:25 +0200

    starting up the invoice controller

---
+++ Server/src/Controllers/invoice.controllers.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-case-declarations */
+const database = require('../Configs/database');
+const logger = require('../Configs/config').logger;
+const Invoice = require('../Models/invoice.models');
